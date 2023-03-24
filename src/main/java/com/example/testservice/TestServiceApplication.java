cf5bc143c953363e01e23a1222a794af2027ea72
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2023-03-24 12:21:12 +0100

    Add endpoint for testing OF type reflection hints.

--- src/main/java/com/example/testservice/TestServiceApplication.java
+++ src/main/java/com/example/testservice/TestServiceApplication.java
@@ -3,6 +3,8 @@ package com.example.testservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -20,6 +22,51 @@ public class TestServiceApplication {
 			return "test";
 		}
 
+		@PostMapping(value = "/test", produces = "application/json", consumes = "application/json")
+		public TestReturnType testTypesForReflection(@RequestBody TestArgType testArg) {
+			return new TestReturnType(testArg.getTest());
+		}
+
 	}
 
+}
+
+class TestReturnType {
+
+	private String test;
+
+	public TestReturnType(String test) {
+		this.test = test;
+	}
+
+	public TestReturnType() {
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+}
+
+class TestArgType {
+
+	private String test;
+
+	public TestArgType(String test) {
+		this.test = test;
+	}
+
+	public TestArgType() {
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
 }
